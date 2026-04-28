b2ed3b19fb361d8f92adb92d4fdea1cea8cb9101
stanley <stanleygomesdasilva@gmail.com>
2026-04-28 05:10:54 -0300

    fix: restore LC_ALL=C to fix sudo prompt translation timeout

--- bootstrap.sh
+++ bootstrap.sh
@@ -40,6 +40,7 @@ echo "🛠️ Running Ansible playbook..."
 cd "$TARGET_DIR"
 
 # Check if Makefile exists to use 'make run', otherwise run ansible-playbook directly
+export LC_ALL=C
 if [ -f "Makefile" ]; then
     make run < /dev/tty
 else
